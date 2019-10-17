9e0c12d6a15fda15dd4668cd65f2b841e366b2fc
mwebber3 <mwebber3@users.noreply.github.com>
2019-10-17 18:46:25 -0400

    Added new files.

---
+++ 4230 Programming Languages/Assignment7/assign7data.pl
@@ -0,0 +1,71 @@
+male(balbo_baggins).
+male(bilbo_baggins).
+male(bungo_baggins).
+male(drogo_baggins).
+male(fosco_baggins).
+male(frodo_baggins).
+male(largo_baggins).
+male(mungo_baggins).
+male(ponto_baggins).
+male(gorbadoc_brandybuck).
+male(meriadoc_brandybuck).
+male(rorimac_brandybuck).
+male(saradoc_brandybuck).
+male(adalgrim_took).
+male(hildigrim_took).
+male(palladin_took_ii).
+male(peregrin_took).
+male(gerontus_took).
+
+female(rosa_baggins).
+female(eglantine_banks).
+female(berylla_boffin).
+female(ruby_bolger).
+female(primula_brandybuck).
+female(mimosa_bunce).
+female(adamanta_chubb).
+female(menegilda_goold).
+female(laura_grubb).
+female(tanta_hornblower).
+female(belladonna_took).
+female(esmerelda_took).
+female(mirabella_took).
+
+parent(drogo_baggins, frodo_baggins).
+parent(primula_brandybuck, frodo_baggins).
+parent(bungo_baggins, bilbo_baggins).
+parent(belladonna_took, bilbo_baggins).
+parent(saradoc_brandybuck, meriadoc_brandybuck).
+parent(esmerelda_took, meriadoc_brandybuck).
+parent(palladin_took_ii, peregrin_took).
+parent(eglantine_banks, peregrin_took).
+parent(adalgrim_took, palladin_took_ii).
+parent(adalgrim_took, esmerelda_took).
+parent(hildigrim_took, adalgrim_took).
+parent(rosa_baggins, adalgrim_took).
+parent(gorbadoc_brandybuck, primula_brandybuck).
+parent(mirabella_took, primula_brandybuck).
+parent(gerontus_took, hildigrim_took).
+parent(adamanta_chubb, hildigrim_took).
+parent(gerontus_took, belladonna_took).
+parent(adamanta_chubb, belladonna_took).
+parent(gerontus_took, mirabella_took).
+parent(adamanta_chubb, mirabella_took).
+parent(rorimac_brandybuck, saradoc_brandybuck).
+parent(menegilda_goold, saradoc_brandybuck).
+parent(gorbadoc_brandybuck, rorimac_brandybuck).
+parent(mirabella_took, rorimac_brandybuck).
+parent(fosco_baggins, drogo_baggins).
+parent(ruby_bolger, drogo_baggins).
+parent(largo_baggins, fosco_baggins).
+parent(tanta_hornblower, fosco_baggins).
+parent(mungo_baggins, bungo_baggins).
+parent(laura_grubb, bungo_baggins).
+parent(balbo_baggins, mungo_baggins).
+parent(berylla_boffin, mungo_baggins).
+parent(balbo_baggins, largo_baggins).
+parent(berylla_boffin, largo_baggins).
+parent(balbo_baggins, ponto_baggins).
+parent(berylla_boffin, ponto_baggins).
+parent(ponto_baggins, rosa_baggins).
+parent(mimosa_bunce, rosa_baggins).
